f7b6230278b1203a079e0a7bf631c069799703f4
Sebastian Vollmer <s.vollmer.4@warwick.ac.uk>
2020-07-24 13:26:35 +0200

    discrete

--- src/simple_discrete.jl
+++ src/simple_discrete.jl
@@ -1,5 +1,5 @@
 using Distributions
-using Plots
+#using Plots
 
 
 # think about prior for parameters- maybe also discrete!
@@ -13,16 +13,34 @@ dgp = NegativeBinomial(r, p)
 real_data = rand(dgp,n_real)
 eval_data= rand(dgp,n_eval)
 real_dataH = rand(dgp,n_realH)
+syn_data =
+
+
+
+function geom_mechanism(v,geom_p=0.5)
+    res=v+rand(Geometric(geom_p))*(2*(rand()>0.5)-1)
+    res*(res>0)+0 # hack
+end
+syn_data = geom_mechanism.(real_dataH)
+
+
 
 #  Geometirc https://drive.google.com/file/d/114GWEJdlEsHa7wKtKFHEnpG56MVQenlq/view?usp=sharing
+
+function lpmfnb(r::Float64,p::Float64,v::Array{Int64})
+    logpdf(NegativeBinomial(r, p),v)
+end
 
 
 prior_params=[[rs,ps] for rs in [1:5;] for ps in [0.1,0.2,0.3]]
 prior_weights= [1.0 for i=1:length(prior_params)]
 
 logpdf(dgp, real_data)
-posterior=
 
-
-
-pdf(NegativeBinomial(prior_params[1]...),1000)
+function posterior(prior_weights,prior_params,data)
+    loglikes=[sum(lpmfnb(pp...,data)) for pp in prior_params]
+    loglikes=loglikes.-minimum(loglikes)
+    posterior_v= prior_weights .* exp.(loglikes)
+    posterior_v=posterior_v/sum(posterior_v)
+    return posterior_v
+end
